cdae034798de44cbf8b3e4e8f0d22d190da84531
Alyssa Crechiolo <alyssacrechiolo@gmail.com>
2020-02-12 09:35:01 -0500

    final commit

--- src/changehandler.js
+++ src/changehandler.js
@@ -14,25 +14,47 @@ class ChangeHandler {
    * The parameter "type" is a string that is either quarter, dime, nickel, or penny
    */
   insertCoin(typeOfCoin) {
-
+    if (typeOfCoin === "quarter") {
+      this.cashTendered += 25;
+    } else if (typeOfCoin === "dime") {
+      this.cashTendered += 10;
+    } else if (typeOfCoin === "nickel") {
+      this.cashTendered += 5;
+    } else if (typeOfCoin === "penny") {
+      this.cashTendered += 1;
+    }
   }
-
   /**
    * Returns true if enough coins have been inserted to at least meet the amountDue
    */
   isPaymentSufficient() {
-      
+    if (this.cashTendered > this.amountDue) {
+      return true;
+    } else if (this.cashTendered < this.amountDue) {
+      return false;
+    } else if (this.cashTendered === this.amountDue) {
+      return true;
+    }
   }
 
   giveChange() {
-    // TODO return the correct change in the following format...
-    return {
-      quarters: 0,
-      dimes: 0,
-      nickels: 0,
-      pennies: 0
-    };
+    let changeDue = { quarters: 0, dimes: 0, nickels: 0, pennies: 0 };
+    while (this.cashTendered > this.amountDue) {
+      if (this.cashTendered - this.amountDue >= 25) {
+        this.cashTendered -= 25;
+        changeDue.quarters++;
+      } else if (this.cashTendered - this.amountDue >= 10) {
+        this.cashTendered -= 10;
+        changeDue.dimes++;
+      } else if (this.cashTendered - this.amountDue >= 5) {
+        this.cashTendered -= 5;
+        changeDue.nickels++;
+      } else {
+        this.cashTendered -= 1;
+        changeDue.pennies++;
+      }
+    }
+    return changeDue;
   }
 }
-
 module.exports = { ChangeHandler };
